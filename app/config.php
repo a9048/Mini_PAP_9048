65f0a6cdaf4105fafeb1fd60125baed42488a646
a9048 <a9048@oficina.pt>
2023-06-27 12:50:58 +0100

    implementação da base de dados

--- app/config.php
+++ app/config.php
@@ -1,12 +1,12 @@
 <?php
-
-    $servername = "localhost";
-    $username = "root"; 
-    $password = ""; 
-    $dbname = "BD_final"; 
-    $conn = new PDO_MYSQL($servername, $username, $password, $dbname);
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $servername = "localhost";
+        $username = "root"; 
+        $password = ""; 
+        $dbname = "BD_final"; 
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
     }
-
 ?>
